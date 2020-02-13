b354c62c5a55b69286331e199977f65b8f36cfcf
Bram Veenboer <bram.veenboer@gmail.com>
2020-02-13 15:27:37 +0100

    Add tStationBeamHamaker
    This test reads a measurement set and produces a beam image
    for all stations.

---
+++ test/beam-helper.cpp
@@ -0,0 +1,127 @@
+#include "beam-helper.h"
+
+#include "fitswriter.h"
+
+void GetPhaseCentreInfo(
+    casacore::MeasurementSet& ms,
+    size_t fieldId,
+    double& ra,
+    double& dec)
+{
+    casacore::MSAntenna aTable = ms.antenna();
+    size_t antennaCount = aTable.nrow();
+    if(antennaCount == 0) throw std::runtime_error("No antennae in set");
+    casacore::MPosition::ScalarColumn antPosColumn(aTable, aTable.columnName(casacore::MSAntennaEnums::POSITION));
+    casacore::MPosition ant1Pos = antPosColumn(0);
+    casacore::MEpoch::ScalarColumn timeColumn(ms, ms.columnName(casacore::MSMainEnums::TIME));
+    casacore::MSField fTable(ms.field());
+    casacore::MDirection::ScalarColumn phaseDirColumn(fTable, fTable.columnName(casacore::MSFieldEnums::PHASE_DIR));
+    casacore::MDirection phaseDir = phaseDirColumn(fieldId);
+    casacore::MEpoch curtime = timeColumn(0);
+    casacore::MeasFrame frame(ant1Pos, curtime);
+    casacore::MDirection::Ref j2000Ref(casacore::MDirection::J2000, frame);
+    casacore::MDirection j2000 = casacore::MDirection::Convert(phaseDir, j2000Ref)();
+    casacore::Vector<casacore::Double> j2000Val = j2000.getValue().get();
+    ra = j2000Val[0];
+    dec = j2000Val[1];
+}
+
+void GetITRFDirections(
+    vector3r_t* itrfDirections,
+    size_t subgrid_size, float image_size,
+    double time, double phaseCentreRA, double phaseCentreDec)
+{
+    float subgrid_pixelsize = image_size / subgrid_size;
+
+    for (size_t y = 0; y < subgrid_size; y++) {
+        for (size_t x = 0; x < subgrid_size; x++) {
+            // IDG uses a flipped coordinate system which is moved by half a pixel:
+            double dl = -subgrid_pixelsize;
+            double dm = -subgrid_pixelsize;
+            double pdl = -0.5*dl;
+            double pdm =  0.5*dm;
+
+            double l, m, n, ra, dec;
+
+            XYToLM<double>(x, y, dl, dm, subgrid_size, subgrid_size, l, m);
+
+            l += pdl;
+            m += pdm;
+            n = sqrt(1.0 - l*l - m*m);
+
+	        vector3r_t _l_vector_itrf;
+	        vector3r_t _m_vector_itrf;
+	        vector3r_t _n_vector_itrf;
+
+	        const casacore::Unit radUnit("rad");
+
+	        LOFAR::StationResponse::ITRFConverter itrfConverter(time);
+
+            casacore::MDirection lDir(casacore::MVDirection(
+            	casacore::Quantity(phaseCentreRA + M_PI/2, radUnit),
+            	casacore::Quantity(0, radUnit)),
+            	casacore::MDirection::J2000);
+            setITRFVector(itrfConverter.toDirection(lDir), _l_vector_itrf);
+
+            casacore::MDirection mDir(casacore::MVDirection(
+            	casacore::Quantity(phaseCentreRA, radUnit),
+            	casacore::Quantity(phaseCentreDec + M_PI/2, radUnit)),
+            	casacore::MDirection::J2000);
+            setITRFVector(itrfConverter.toDirection(mDir), _m_vector_itrf);
+
+            casacore::MDirection nDir(casacore::MVDirection(
+            	casacore::Quantity(phaseCentreRA, radUnit),
+            	casacore::Quantity(phaseCentreDec, radUnit)),
+            	casacore::MDirection::J2000);
+            setITRFVector(itrfConverter.toDirection(nDir), _n_vector_itrf);
+
+            vector3r_t itrfDirection;
+
+            itrfDirection[0] = l*_l_vector_itrf[0] + m*_m_vector_itrf[0] + n*_n_vector_itrf[0];
+            itrfDirection[1] = l*_l_vector_itrf[1] + m*_m_vector_itrf[1] + n*_n_vector_itrf[1];
+            itrfDirection[2] = l*_l_vector_itrf[2] + m*_m_vector_itrf[2] + n*_n_vector_itrf[2];
+
+            itrfDirections[y * subgrid_size + x] = itrfDirection;
+        }
+    }
+}
+
+void StoreATermsReal(
+    const std::string& filename,
+    const std::complex<float>* buffer,
+    size_t nStations,
+    size_t width,
+    size_t height)
+{
+	size_t ny = floor(sqrt(nStations)), nx = (nStations+ny-1) / ny;
+    std::cout << "Storing " << filename << " (" << nStations << " ant, " << nx << " x " << ny << ")\n";
+    std::vector<double> img(nx*ny * width*height, 0.0);
+	for(size_t ant=0; ant!=nStations; ++ant)
+	{
+        typedef std::complex<float> Data[nStations][height][width][4];
+        Data* data_ptr = (Data *) buffer;
+
+		size_t xCorner = (ant%nx)*width, yCorner = (ant/nx)*height;
+		for(size_t y=0; y!=height; ++y)
+		{
+			for(size_t x=0; x!=width; ++x)
+			{
+				std::complex<float> xx = (*data_ptr)[ant][y][x][0];
+				img[(yCorner + y)*width*nx + x + xCorner] = xx.real();
+			}
+		}
+	}
+	FitsWriter writer;
+	writer.SetImageDimensions(nx*width, ny*height);
+	writer.Write(filename, img.data());
+}
+
+void setITRFVector(
+    const casacore::MDirection& itrfDir,
+    vector3r_t& itrf)
+{
+    const casacore::Vector<double>& itrfVal = itrfDir.getValue().getValue();
+    itrf[0] = itrfVal[0];
+    itrf[1] = itrfVal[1];
+    itrf[2] = itrfVal[2];
+}
---
+++ test/beam-helper.h
@@ -0,0 +1,71 @@
+#include <StationResponse/ITRFDirection.h>
+#include <StationResponse/ITRFConverter.h>
+#include <StationResponse/ElementResponse.h>
+#include <StationResponse/Station.h>
+#include <StationResponse/LofarMetaDataUtil.h>
+
+#include <casacore/measures/Measures/MPosition.h>
+#include <casacore/measures/Measures/MEpoch.h>
+#include <casacore/measures/Measures/MCDirection.h>
+#include <casacore/measures/TableMeasures/ScalarMeasColumn.h>
+#include <casacore/measures/TableMeasures/ArrayMeasColumn.h>
+#include <casacore/ms/MeasurementSets/MSAntenna.h>
+#include <casacore/tables/Tables/TableKeyword.h>
+#include <casacore/tables/Tables/TableRecord.h>
+#include <casacore/tables/Tables/ScalarColumn.h>
+
+using matrix22c_t = LOFAR::StationResponse::matrix22c_t;
+using vector3r_t = LOFAR::StationResponse::vector3r_t;
+
+void GetPhaseCentreInfo(
+    casacore::MeasurementSet& ms,
+    size_t fieldId,
+    double& ra,
+    double& dec);
+
+void GetITRFDirections(
+    vector3r_t* itrfDirections,
+    size_t subgrid_size,
+    float image_size,
+    double time,
+    double phaseCentreRA,
+    double phaseCentreDec);
+
+void StoreATermsReal(
+    const std::string& filename,
+    const std::complex<float>* buffer,
+    size_t nStations,
+    size_t width,
+    size_t height);
+
+void setITRFVector(
+    const casacore::MDirection& itrfDir,
+    vector3r_t& itrf);
+
+inline matrix22c_t operator*(
+    const matrix22c_t &arg0,
+    const matrix22c_t &arg1)
+{
+    matrix22c_t result;
+    result[0][0] = arg0[0][0] * arg1[0][0] + arg0[0][1] * arg1[1][0];
+    result[0][1] = arg0[0][0] * arg1[0][1] + arg0[0][1] * arg1[1][1];
+    result[1][0] = arg0[1][0] * arg1[0][0] + arg0[1][1] * arg1[1][0];
+    result[1][1] = arg0[1][0] * arg1[0][1] + arg0[1][1] * arg1[1][1];
+    return result;
+}
+
+template<typename T>
+void XYToLM(
+    size_t x,
+    size_t y,
+    T pixelSizeX,
+    T pixelSizeY,
+    size_t width,
+    size_t height,
+    T &l,
+    T &m)
+{
+	T midX = (T) width / 2.0, midY = (T) height / 2.0;
+	l = (midX - (T) x) * pixelSizeX;
+	m = ((T) y - midY) * pixelSizeY;
+}
