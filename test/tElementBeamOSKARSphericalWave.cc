6dff1c9d161fd26274a2338193b1b826a4393c7a
Bram Veenboer <bram.veenboer@gmail.com>
2020-02-14 16:41:43 +0100

    Use 140 Mhz as default frequency for OSKAR tests

--- test/tElementBeamOSKARSphericalWave.cc
+++ test/tElementBeamOSKARSphericalWave.cc
@@ -7,7 +7,7 @@ using namespace LOFAR::StationResponse;
 int main(int argc, char** argv)
 {
     ElementResponseModel model(OSKARSphericalWave);
-    double frequency = 132e6; // Mhz
+    double frequency = 140e6; // Mhz
     std::string input_filename(TEST_MEASUREMENTSET);
     std::string output_filename("element-beams-oskar-sphericalwave.fits");
     run(model, frequency, input_filename, output_filename);
