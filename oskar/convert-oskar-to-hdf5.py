c2b03c269ef59730131cc83511e0f99162557ab1
Bram Veenboer <bram.veenboer@gmail.com>
2020-02-21 14:11:28 +0100

    Rewrite of convert-oskar-to-hdf5 script
    The layout in which the original script wrote the coefficients was
    incorrect. This new version writes the coefficients in the format
    expected by the oskar_evaluate_spherical_wave_sum method: as a compact
    rectangular matrix with only nonzeros.

--- oskar/convert-oskar-to-hdf5.py
+++ oskar/convert-oskar-to-hdf5.py
@@ -12,7 +12,6 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument("input_dir", type=str, help="directory containing coefficients")
 parser.add_argument("output_file", type=str, help="name of the HDF5 output file")
-parser.add_argument("-lmax", type=int, help="select the number of coefficients in one dimension")
 parser.add_argument("-debug", dest="debug", action="store_true")
 parser.set_defaults(input_dir=".")
 parser.set_defaults(output_file="oskar.h5")
@@ -28,9 +27,6 @@ dirs = os.listdir(input_dir)
 dirs = list(filter(lambda x: x.isdigit(), dirs))
 freqs = sorted(map(int, dirs))
 print("Parsing frequencies %d-%d MHz" % (int(freqs[0]), int(freqs[-1])))
-
-# Select the upper-left rectangle of size l_max * l_max
-l_max = args.lmax
 
 # Create HDF5 file
 output_file = args.output_file
@@ -61,51 +57,46 @@ for freq in tqdm(freqs):
 
     # Make sure that all four sub-matrices are shaped identically
     assert(alpha_te_a.shape == alpha_tm_a.shape == alpha_te_b.shape == alpha_tm_b.shape)
+
+    # Get dimensions of input matrix
+    # This matrix is lower triangular, e.g.:
+    # 32x65 (2080 elements), with 1088 (32x(32+2)) nonzeros
     shape = alpha_te_a.shape
     nr_elements = shape[0]
-    nr_coeffs_y = shape[1]
-    nr_coeffs_x = shape[2]
+    src_height = shape[1]
+    src_width = shape[2]
     if (debug):
         print("nr_elements: ", nr_elements)
-        print("nr_coeffs_y: ", nr_coeffs_y)
-        print("nr_coeffs_x: ", nr_coeffs_x)
+        print("input height: ", src_height)
+        print("input width: ", src_width)
 
-    # Select coeffs
-    if (l_max and
-        l_max <= nr_coeffs_y and
-        l_max <= nr_coeffs_y):
-        alpha_te_a = alpha_te_a[:,:l_max,:l_max]
-        alpha_tm_a = alpha_tm_a[:,:l_max,:l_max]
-        alpha_te_b = alpha_te_b[:,:l_max,:l_max]
-        alpha_tm_b = alpha_tm_b[:,:l_max,:l_max]
-        shape = alpha_te_a.shape
+    # Determine dimensions of output matrix
+    # This matrix is rectangular and contains
+    # all nonzero elements of the input matrix
+    dst_height = src_height + 2
+    dst_width = src_height
+    if (debug):
+        print("output height: ", dst_height)
+        print("output width: ", dst_width)
 
-    # Put coeffs in one matrix
-    data = np.array([alpha_te_a, alpha_tm_a, alpha_te_b, alpha_tm_b])
-    # [tetm*pol,ant,l,m]
-    if (debug): print(data.shape)
+    # Fill the output matrix
+    # The inner dimension are set as follows:
+    # (x_te_re, x_te_im), (x_tm_re, x_tm_im),
+    # (y_te_re, y_te_im), (y_tm_re, y_tm_im)
+    data = np.zeros((nr_elements, dst_height * dst_width, 4), dtype=np.complex128)
 
-    # Move pol+tetm axis inside
-    data = data.transpose((1,2,3,0))
-    # [ant,ant,l,m,tetm*pol]
-    if (debug): print(data.shape)
-
-    # Split tetm and polarization axis
-    s = data.shape
-    assert(s[3] == 4)
-    s = (s[0:3]) + (2,2)
-    data = data.reshape(s)
-    # [ant,l,m,pol,tetm]
-    if (debug): print(data.shape)
-
-    # Sanity check
-    assert(np.allclose(data[:, :, :, 0, 0], alpha_te_a))
-    assert(np.allclose(data[:, :, :, 1, 0], alpha_te_b))
-    assert(np.allclose(data[:, :, :, 1, 1], alpha_tm_b))
-    assert(np.allclose(data[:, :, :, 0, 1], alpha_tm_a))
+    i = 0
+    for m in range(0,src_height):
+        for l in range((m+1)*2, -1, -1):
+            ind0 = l * src_width + m
+            data[:,i,0] = alpha_te_a[:,m,l]
+            data[:,i,1] = alpha_tm_a[:,m,l]
+            data[:,i,2] = alpha_te_b[:,m,l]
+            data[:,i,3] = alpha_tm_b[:,m,l]
+            i += 1
 
     # Add group for current frequency
-    hf.create_dataset(str(freq), s, data=data)
+    hf.create_dataset(str(freq), data.shape, data=data)
 
 # Close HDF5 file
 hf.close()
