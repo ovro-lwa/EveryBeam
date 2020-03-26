0ff75741c318f906f22c6d4a297fd249eb98a770
Bram Veenboer <bram.veenboer@gmail.com>
2020-03-26 12:31:39 +0100

    Extract testdata in Dockerfile rather than in run-test.sh
    This way, the test pipeline does not need to run any git commands.

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/lofarbeam/lib
 export PATH=$PATH:/opt/dp3/bin
-cd ${TESTDIR} && tar xf ${INSTALLDIR}/dp3/DP3-${DP3_VERSION}/DPPP/test/tNDPPP-generic.in_MS.tgz
 cd ${TESTDIR} && DPPP msin=tNDPPP-generic.MS msout=. steps=[predict] predict.usebeammodel=True predict.elementmodel=oskardipole predict.sourcedb=tNDPPP-generic.MS/sky
