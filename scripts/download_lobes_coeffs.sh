e92bde9629f56a28130d28f13ecfbc67c68420ca
Andre Offringa <offringa@astron.nl>
2020-10-24 11:56:21 +0200

    Fix for several cmake errors in lobes stuff

--- scripts/download_lobes_coeffs.sh
+++ scripts/download_lobes_coeffs.sh
@@ -16,4 +16,4 @@ mkdir -p coeffs/lobes
 cd coeffs/lobes
 
 # Download the h5 coefficient files in case they're not present (-nc option) in the coeffs/lobes directory
-wget -q -r -nc -nH --no-parent --cut-dirs=3 --accept-regex=LOBES_.*\.h5 --reject index.html -e robots=off https://www.astron.nl/citt/EveryBeam/lobes/
+wget -q -r -nc -nH --no-check-certificate --no-parent --cut-dirs=3 --accept-regex=LOBES_.*\.h5 --reject index.html -e robots=off https://www.astron.nl/citt/EveryBeam/lobes/
