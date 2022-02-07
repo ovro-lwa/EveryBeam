b41aaebac78ebb17c04c8d18e5d6f69ba5da2f34
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2022-02-07 15:27:40 +0000

    Update downloading lobes coefficients

--- scripts/download_lobes_coeffs.sh
+++ scripts/download_lobes_coeffs.sh
@@ -5,14 +5,10 @@
 # Author: Jakob Maljaars
 # Email: jakob.maljaars_@_stcorp.nl
 
-# Script for downloading and extracting the LOBES coefficients
-# When invoked from within CMAKE, the WORKING_DIRECTORY is
-# assumed to be the CMAKE_BINARY_DIR
+# Script for downloading and extracting the LOBES coefficients into
+# the current directory. When invoked from within CMake, CMake should set
+# the proper WORKING_DIRECTORY.
 set -e
-
-# Make directory coeffs/lobes in source directory
-mkdir -p coeffs/lobes
-cd coeffs/lobes
 
 # Download the h5 coefficient files in case they're not present (-nc option) in the coeffs/lobes directory
 wget -q -r -nc -nH --no-check-certificate --no-parent --cut-dirs=3 --accept-regex=LOBES_.*\.h5 --reject index.html -e robots=off https://www.astron.nl/citt/EveryBeam/lobes/
